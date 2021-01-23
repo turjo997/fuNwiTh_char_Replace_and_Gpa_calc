7a54b1f6e9d766286d53513e15d9ab651e772dae
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-01-23 23:00:25 +0600

    Create gpa.php

---
+++ gpa.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+
+<html>
+     <head>
+	 
+	        <title>Form</title>
+			
+			<link rel = "stylesheet" href="gpa_style.css"/>	
+			
+				<style>
+	
+	</style>
+			<meta charset = "UTF-8">
+		
+	 </head>
+	 
+	  </head>
+	 
+	 <body>
+	 
+	 
+	        <form method = "post">
+			 
+		      
+			<fieldset>
+			
+			<legend align = "center">FILL THE GRADE LIST !!!</legend>
+			
+			
+			<label for = "First Number"> Economics and Accounting:</label> 
+			<input type = "text" name = "first_no">
+			 
+			 <br>
+			 <br>
+			 
+			 <label for = "Second Number"> Mathematical Analysis for Computer Science  :</label> 
+			 <input type = "text" name = "second_no">
+			 
+			 <br>
+			 <br>
+			 
+			 <label for = "Third Number"> Database  :</label> 
+			 <input type = "text" name = "third_no">
+			 
+			 <br>
+			 <br>
+			 
+			 <label for = "Four Number"> Microprocessors  :</label> 
+			 <input type = "text" name = "four_no">
+			 
+			 	 <br>
+			 <br>
+			 
+			 <label for = "Fifth Number"> Digital System Design :</label> 
+			 <input type = "text" name = "fifth_no">
+			 
+			 
+			  <label for = "Sixth Number"> Software Development-IV :</label> 
+			 <input type = "text" name = "sixth_no">
+			 
+			 <br>
+			 <br>
+			 
+			 <label for = "Seventh umber"> Database Lab :</label> 
+			 <input type = "text" name = "seventh_no">
+			 
+			 <br>
+			 <br>
+			 
+			 <label for = "Eight Number"> Microprocessors Lab:</label> 
+			 <input type = "text" name = "eight_no">
+			 
+			 <br>
+			 <br>
+			 
+			 <label for = "Nine Number"> Digital System Design Lab :</label> 
+			 <input type = "text" name = "nine_no">
+			 
+			 <input  type="submit" name="submit" value="FIND GPA">
+			 
+			 <?php
+				include_once 'add.php';
+			 ?>
+			 
+
+
+
+
+			</fieldset>
+			  
+			 </form>
+			 
+			
+			
+			 
+			 
+	 </body>
+
+
+
+</html>
+
